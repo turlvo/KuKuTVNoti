545d24401628a2629e42a087cc4dede9fe445c8c
KuKu <turlvo@gmail.com>
2018-03-06 22:30:34 +0900

    changed lgtv.js

--- KuKuTVNoti-api/lgtv.js
+++ KuKuTVNoti-api/lgtv.js
@@ -5,16 +5,17 @@ var express = require('express');
 var router = express.Router();
 
 // test route to make sure everything is working (accessed at GET /lgtv/:ip/noti/:message)
-router.post('/:ip/noti/:message', function(req, res) {
+router.post('/:ip/noti', function(req, res) {
     console.log('request : ' + req.url)
     res.json({ message: 'hooray! welcome to our api!' });
     var lgtv = require("lgtv2")({
-        url: 'ws://' + req.params.ip + ':3000'
+        url: 'ws://' + req.params.ip + ':3000',
+        keyFile: '/keys/lgtv-'
     });
 
     lgtv.on('connect', function () {
         console.log('connected');
-        lgtv.request('ssap://system.notifications/createToast', {message: req.params.message});
+        lgtv.request('ssap://system.notifications/createToast', {message: req.body["msg"]});
         lgtv.disconnect();
 
 
@@ -25,16 +26,13 @@ router.post('/:ip/noti/:message', function(req, res) {
 router.get('/:ip/service-list', function(req, res) {
     console.log('ip :' + req.params.ip)
     var lgtv = require("lgtv2")({
-        url: 'ws://' + req.params.ip + ':3000'
+        url: 'ws://' + req.params.ip
     });
-
-    lgtv.on('connect', function () {
-        lgtv.request('ssap://audio/getStatus', function (err, response) {
-            console.log('response', response);
-            response.send("noti");
-        });
+    lgtv.request('ssap://audio/getStatus', function (err, response) {
+        console.log('response', response);
     });
 });
 // more routes for our API will happen here
 
 module.exports = router;
+
